39baa67f9c0bf5d49858270cde2bec2cb0b78d30
Brittanykusi <brittany.kusi-gyabaah@stonybrook.edu>
2022-11-18 20:51:10 -0500

    updates

---
+++ APR.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+
+# load data
+LAST_VISIT_DATE_COLUMN = 'date'
+DATA = ('/Users/brittanykusi-gyabaah/Documents/dashboard-streamlit/streamlit_data.csv')
+
+def load_data(nrows):
+    data = pd.read_csv(DATA, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[LAST_VISIT_DATE_COLUMN] = pd.to_datetime(data[LAST_VISIT_DATE_COLUMN])
+    return data
+
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+# Load 10,000 rows of data into the dataframe.
+# @st.cache
+data = load_data(10000)
+# Notify the reader that the data was successfully loaded.
+data_load_state.text("Done! (using st.cache)")
+
+# title
+st.title('Active Patient Report')
+#subheader
+st.subheader('Raw data')
+st.write(data)
+
+
+
